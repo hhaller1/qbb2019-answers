7655b49e6897bfc70bacce133352103834afaa8a
hhaller1 <hhaller1@jhu.edu>
2019-08-28 20:10:17 -0400

    fixed he day 3

--- day3-homework/kmer_matcher.py
+++ day3-homework/kmer_matcher.py
@@ -1,34 +1,28 @@
 #!/usr/bin/env python3
-
-"""match all kmers in a FASTA file"""
+"""match extender"""
 
 from fasta import FASTAReader
-#import the fasta file to parse FASTA files- imports only the FASTAReader
-
 import sys
-target = FASTAReader(open(sys.argv[1]))
-query = FASTAReader(open(sys.argv[2]))
+
+target = FASTAReader(open(sys.argv[1])) #subset.fa
+query = FASTAReader(open(sys.argv[2])) #droYak2_seq.fa
 k = int(sys.argv[3])
 
 kmers_from_target = {}
 
 for ident, sequence in target:
-    sequence = sequence.upper()
-    for i in range(0, len(sequence)- k + 1):  ## i is position ident is identity
-        kmertarget = sequence[i:i+k]
-        if kmertarget not in kmers_from_target:
-            kmers_from_target[kmertarget]= [(ident, i)]
-        else:
-            kmers_from_target[kmertarget].append((ident, i))
-         
-    
-for ident1, sequence1 in query:
-    sequence1 = sequence1.upper()
-    for i1 in range(0, len(sequence1)- k + 1):
-        kmerquery = sequence1[i1:i1+k]
-        if kmerquery not in kmers_from_target:
-            continue
-        else:
-            print (kmers_from_target[kmerquery], str(i1), kmerquery)
-    
-    
+  sequence = sequence.upper()
+  for i in range(0, len(sequence) - k +1):
+      kmer = sequence[i:i+k]
+      if kmer in kmers_from_target:
+          kmers_from_target[kmer].append((ident,i))
+      else:
+          kmers_from_target[kmer]=[(ident,i)]
+
+for ident, sequence in query:
+  sequence = sequence.upper()
+  for i in range(0, len(sequence) - k +1):
+      kmer = sequence[i:i+k]
+      if kmer in kmers_from_target:
+          for ident, j in kmers_from_target[kmer]:
+              print(ident, j, i, kmer)
